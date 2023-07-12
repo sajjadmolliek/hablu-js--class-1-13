afe3245544a2a5de195612a3a1e155aea6757405
sajjadmolliek <sajjadmolliek2018@gmail.com>
2023-07-13 04:17:09 +0600

    first commit

---
+++ hablu01.js
@@ -0,0 +1,31 @@
+var friendName = ("Sajjad Molliek");
+console.log(friendName);
+
+var strType = "hablu Programmer";
+console.log(typeof strType);
+
+var numType = 1734402046;
+console.log( numType);
+
+ var boolType = true;
+ console.log(typeof boolType);
+
+ var rochona = "lorem is a gadha is not a matha batha ...amar mama kutta tar nai vutta.. kaki holo soitan kakir  nai potan. kakar sala bolod se holo goru ";
+
+ console.log(rochona.toUpperCase());
+
+ var myFnd = "sajjad  jeni  saju  janifa  axsa";
+console.log( myFnd.split("axsa"));
+console.log( myFnd.indexOf("janifa"));
+
+var num1 = "55.557";
+var num2 = 99
+num1 = parseFloat(num1);
+
+var total = num1+num2;
+
+console.log(total);
+
+num1 = " "+num1;
+var total = num1+num2;
+console.log(total);
